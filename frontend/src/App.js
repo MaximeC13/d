cfbe70440cb64d1b61fb8bfec4b1f6d656347420
admin <you@example.com>
2022-03-30 16:47:08 +0200

    With list products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/">coach life</a>
+        <a href="/">Natacha life</a>
       </header>
-      <main>Hello world</main>
+      <main>
+        <h1>Liste des Produits :</h1>
+        <div className="products">
+          {data.product.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  Prix : <strong>{product.price} € </strong>
+                </p>
+                <button>Ajouter au panier</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
