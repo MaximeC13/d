749dfe697261796d77400e8f9fa3c15db0a1e890
admin <you@example.com>
2022-03-30 15:24:52 +0200

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">coach life</a>
+      </header>
+      <main>Hello world</main>
+    </div>
+  );
+}
+
+export default App;
